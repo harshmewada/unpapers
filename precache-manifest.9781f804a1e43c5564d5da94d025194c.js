1d1261bdfdd882eb8b4ea6ee016383c937212a89
harshmewada <harshmewada056@gmail.com>
2019-09-05 13:11:10 +0530

    Updates

---
+++ precache-manifest.9781f804a1e43c5564d5da94d025194c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78d48cb63048827ea74a568f26660868",
+    "url": "/unpapers/index.html"
+  },
+  {
+    "revision": "6ff2a259cb2b2e480e1a",
+    "url": "/unpapers/static/css/main.c1b59d2f.chunk.css"
+  },
+  {
+    "revision": "23d085bc48f1eccf8d02",
+    "url": "/unpapers/static/js/2.4afce1e5.chunk.js"
+  },
+  {
+    "revision": "6ff2a259cb2b2e480e1a",
+    "url": "/unpapers/static/js/main.b7925874.chunk.js"
+  },
+  {
+    "revision": "87af484c7d07f3def86f",
+    "url": "/unpapers/static/js/runtime~main.f564a6ed.js"
+  },
+  {
+    "revision": "716c18906f2c47106867e1b509aa428e",
+    "url": "/unpapers/static/media/lines.716c1890.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/unpapers/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ac92e64b87e07eef72a1c44e6494bf93",
+    "url": "/unpapers/static/media/menu.ac92e64b.svg"
+  },
+  {
+    "revision": "92f8525226f71be86e1dfd6f27857f0e",
+    "url": "/unpapers/static/media/navbg.92f85252.png"
+  }
+]);
